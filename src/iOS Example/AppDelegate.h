3184b18f8c99c64ad68d8cf309280b8463b9a9cd
Brian Romanko <brian@bigbigbomb.com>
2012-04-02 11:24:03 -0500

    Initial workspace structure

---
+++ src/iOS Example/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOSExample
+//
+//  Created by Brian Romanko on 4/2/12.
+//  Copyright (c) 2012 BigBig Bomb, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
